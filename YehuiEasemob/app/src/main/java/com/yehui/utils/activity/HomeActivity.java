d8f46177a7885a713f8d515100dd457082099db0
yehuijifeng <928186846@qq.com>
2016-01-29 15:25:52 +0800

    改善百度定位，增加环信客服

---
+++ YehuiEasemob/app/src/main/java/com/yehui/utils/activity/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.yehui.utils.activity;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yehui.utils.activity.base.BaseViewPagerActivity;
+
+/**
+ * Created by yehuijifeng on 2016/1/29.
+ */
+public class HomeActivity extends BaseViewPagerActivity {
+    @Override
+    protected View setTabView(ViewGroup container, int position) {
+        return null;
+    }
+
+    @Override
+    protected void setContentView() {
+
+    }
+
+    @Override
+    protected String setTitleText() {
+        return null;
+    }
+}
